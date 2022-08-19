1f7cb0373e0b11430fede47614b58b91f40d059a
Mr.Salih <salihbaz07@gmail.com>
2022-08-19 18:12:38 +0300

    caesar cipher add

--- caesar.c
+++ caesar.c
@@ -2,13 +2,14 @@
 #include <cs50.h>
 #include <ctype.h>
 #include <string.h>
+#include <stdlib.h>
 
 
 int     main(int argc, string argv[])
 {
     if (argc != 2)
     {
-        printf("Usage: ./caesar ket");
+        printf("Usage: ./caesar key\n");
         return 1;
     }
 
@@ -16,9 +17,32 @@ int     main(int argc, string argv[])
     {
         if (!isdigit(argv[1][i]))
         {
-            printf("Usage: ./caesar key");
+            printf("Usage: ./caesar  key\n");
             return 1;
-        }        
+        }     
+
+        int k = atoi(argv[1]);
+
+        string plaintext = get_string("Plaintext: ");
+        printf("Ciphertext: ");
+
+        for (int j = 0; j < strlen(plaintext); j++)
+        {
+            if (isupper(plaintext[j]))
+            {
+                printf("%c", (plaintext[j] - 65 + k) % 26 + 65);
+            }
+            else if (islower(plaintext[j]))
+            {
+                printf("%c", (plaintext[j] - 65 + k) % 26 + 65);
+            }
+            else
+            {
+                printf("%c", plaintext[j]);
+            }
+        }
+        
+        printf("\n");
     }
     
     
