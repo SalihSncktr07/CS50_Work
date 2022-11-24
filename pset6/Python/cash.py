888f7391ba181a917c9edf460ca2a58a2f3d701f
salihprnz07 <salihbaz07@gmail.com>
2022-11-24 15:30:57 +0300

    cash

---
+++ pset6/Python/cash.py
@@ -0,0 +1,17 @@
+while True:         #ask user for input, keep trying for wrong entries
+  try:
+    c = float (input ("Change owed: "))
+  except ValueError:
+    continue
+  if c < 0:
+    continue
+
+  else:   #assign variables for coins, solve the math
+    cents = round (c * 100)
+    quarters = int (cents/25)
+    dimes = int ((cents - (quarters * 25)) / 10)
+    nickels = int ((cents - (quarters * 25 + dimes * 10)) / 5)
+    pennies = int (cents - (quarters * 25 + dimes * 10 + nickels * 5))
+    total = int (quarters + dimes + nickels + pennies)
+    print (f'{total}')
+    break
